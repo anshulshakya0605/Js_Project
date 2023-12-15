a5f13869109da43c830e70842ed25f4094d04841
anshulshakya0605 <anshulshakya0605@gmail.com>
2023-12-15 14:44:58 +0530

    project 3 js

--- 3-DigitalClock/script.js
+++ 3-DigitalClock/script.js
@@ -1,3 +1,10 @@
-const clocl = document.getElementById('clock');
+const clock = document.getElementById('clock');
 // document.querySelector('clock')
 
+
+
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000)
