a34fbca16ab639166b1ee7969dcbfc310af104cd
anshulshakya0605 <anshulshakya0605@gmail.com>
2023-12-20 18:00:47 +0530

    Project 6 add

---
+++ 6-unlimitedColors/script.js
@@ -0,0 +1,11 @@
+// generate a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for(i = 0; i < 6; i++){
+
+    }
+}
+
+console.log(Math.random() * 16)
