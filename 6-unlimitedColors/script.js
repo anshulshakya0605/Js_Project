7fb65c0ab9fb7b653e46cc3f5f06e8e7521720f3
anshulshakya0605 <anshulshakya0605@gmail.com>
2023-12-20 19:01:04 +0530

    update

--- 6-unlimitedColors/script.js
+++ 6-unlimitedColors/script.js
@@ -1,11 +1,28 @@
 // generate a random color
 
 const randomColor = function(){
-    const hex = "0123456789ABCDEF"
-    let color = "#"
-    for(i = 0; i < 6; i++){
-
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
     }
-}
+    return color;
+};
+let intervalId
 
-console.log(Math.random() * 16)
+const startChangingColor = function () {
+intervalId = setInterval(changeBgColor, 1000)
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+const stopChangingColor = function () {
+
+    clearInterval(intervalId)
+};
+
+document.querySelector('#Start').addEventListener('click', startChangingColor);
+
+document.querySelector('#Stop').addEventListener('click', stopChangingColor);
+
+
